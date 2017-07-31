959d81f52bcc1bdc9773ad0ef3b2638bbb54d73d
Vkliao <vkliao@gmail.com>
2017-07-30 20:16:15 -0500

    Correct Thetas 4,5,6 and cleaned up code

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -24,7 +24,6 @@ d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
 a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
 alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
 
-
 # Modified DH params
 s = {alpha0:     0,  a0:      0, d1:  0.75,
      alpha1: -pi/2,  a1:   0.35, d2:     0,  q2: q2-pi/2,
@@ -33,7 +32,6 @@ s = {alpha0:     0,  a0:      0, d1:  0.75,
      alpha4:  pi/2,  a4:      0, d5:     0,
      alpha5: -pi/2,  a5:      0, d6:     0,
      alpha6:     0,  a6:      0, d7: 0.303,  q7: 0}
-
 
 # Define Modified DH Transformation matrix
 T0_1 = Matrix([[             cos(q1),            -sin(q1),            0,              a0],
@@ -78,7 +76,6 @@ T6_G = Matrix([[             cos(q7),            -sin(q7),            0,        
                [                   0,                   0,            0,               1]])
 T6_G = T6_G.subs(s)
 
-
 # Create individual transformation matrices
 T0_2 = (T0_1 * T1_2)  # baselink to link2
 T0_3 = (T0_2 * T2_3)  # baselink to link3
@@ -88,7 +85,9 @@ T0_6 = (T0_5 * T5_6)  # baselink to link6
 T3_6 = (T3_4 * T4_5 * T5_6)
 T0_G = (T0_6 * T6_G)  # baselink to linkG
 #print("T0_G = ",T0_G)
-
+#print('T0_3 = ', T0_3.evalf(subs={q1: 0, q2: 0, q3: 0}))
+#print('T3_6 = ', T3_6.evalf(subs={q4: 0, q5: 0, q6: 0})) 
+#print("\n")
 
 ### Correction needed to account for orientation difference between
 #    definition of Gripper link in urdf vs. DH Convention
@@ -119,9 +118,6 @@ R_corr = (R_z * R_y)
 #    with orientation correction applied
 T_total = simplify(T0_G * R_corr)
 #print('T_total = ', T_total.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
-#print('T0_3 = ', T0_3.evalf(subs={q1: 0, q2: 0, q3: 0}))
-#print('T3_6 = ', T3_6.evalf(subs={q4: 0, q5: 0, q6: 0})) 
-#print("\n")
 #print('T_total = ', T_total)
 
 
@@ -133,56 +129,57 @@ def handle_calculate_IK(req):
     else:
         # Initialize service response
         joint_trajectory_list = []
-        for x in xrange((len(req.poses)-1), len(req.poses)):
+        for x in xrange((len(req.poses)-3), len(req.poses)):
             # IK code starts here
             joint_trajectory_point = JointTrajectoryPoint()
 
-            # Extract end-effector position and orientation from request
-	    # px,py,pz = end-effector position
-	    # roll, pitch, yaw = end-effector orientation
-            px = (req.poses[x].position.x - 0.303) #WC coord
+            # Extract end-effector position = px,py,pz
+            px = (req.poses[x].position.x - 0.325) #simple correction for Wrist Center
             py = req.poses[x].position.y
             pz = req.poses[x].position.z
 	    #print('px = ',px)	
 	    #print('py = ',py)	
 	    #print('pz = ',pz)	
+	    #print("\n")
 
+            # Calculate joint angles using Geometric IK method
+	    rtd = 180/pi  #radians to degrees
+
+            #Solve for end-effector POSITION - theta 1, 2, 3
+	    theta1 = atan2(py, px) 
+            #th1deg = simplify(theta1*rtd)
+	    #print('theta1 = ', th1deg)
+
+            pf = (px**2 + py**2)**0.5
+	    theta2 = (atan2(pf,pz)-acos((1.5**2-1.196**2-(pf-0.35)**2-(pz-0.75)**2)
+			/(-2*1.5*(((pf-0.35)**2+(pz-0.75)**2)**0.5))))+.22
+            #th2deg = theta2*rtd
+	    #print('theta2 = ', simplify(th2deg))
+
+            D = (((pf-0.35)**2 + (pz-0.75)**2 - 1.196**2 - 1.5**2)/(2*1.196*1.5))
+	    theta3 = -pi/2 + acos(D)+.2
+            #th3deg = theta3*rtd
+	    #print('theta3 = ', simplify(th3deg))
+	    #print("\n")
+
+
+            #Decouple the wrist to solve for end-effector ORIENTATION - theta 4, 5, 6
+            R0_3 = (T0_3).evalf(subs={q1:theta1,q2:theta2,q3:theta3}) 
+            #print'R0_3 = ', simplify(R0_3)
+	    #print("\n")
+
+            I0_3 = R0_3**-1
+            #print'I0_3 = ', simplify(I0_3)
+	    #print("\n")
+
+            # Extract end-effector orientation = roll, pitch, yaw  
             (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
                 [req.poses[x].orientation.x, 
 		 req.poses[x].orientation.y,
                  req.poses[x].orientation.z, 
 		 req.poses[x].orientation.w])
-	    print('roll = ',roll, 'pitch = ',pitch, 'yaw = ',yaw)
-
-            # Calculate joint angles using Geometric IK method
-	    rtd = 180/pi
-
-	    theta1 = atan2(py, px)
-            th1deg = simplify(theta1*rtd)
-	    #print('theta1 = ', th1deg)
-            pf = (px**2 + py**2)**0.5
-
-	    theta2 = atan2(pf,pz)-acos((1.5**2-1.196**2-(pf-0.35)**2-(pz-0.75)**2)
-			/(-2*1.5*(((pf-0.35)**2+(pz-0.75)**2)**0.5)))
-	    #theta2 = atan2(pz,pf)+acos((1.5**2-1.196**2-(pf-0.35)**2-(pz-0.75)**2)/(2*1.5*(((pf-0.35)**2+(pz-0.75)**2)**0.5)))
-            th2deg = theta2*rtd
-	    #print('theta2 = ', simplify(th2deg))
-
-            D = (((pf-0.35)**2 + (pz-0.75)**2 - 1.196**2 - 1.5**2)/(2*1.196*1.5))
-	    theta3 = -pi/2 + acos(D)
-            th3deg = theta3*rtd
-	    #print('theta3 = ', simplify(th3deg))
+	    #print'roll = ',roll, 'pitch = ',pitch, 'yaw = ',yaw
 	    #print("\n")
-
-	    #print("\n")
-	    #theta4 = 0
-	    #theta5 = 0
-	    #theta6 = 0
-
-            R0_3 = (T0_3).evalf(subs={q1:theta1,q2:theta2,q3:theta3})  #[0:3,0:3]
-            print('R0_3 = ', simplify(R0_3))
-            I0_3 = R0_3**-1
-            print('I0_3 = ', simplify(I0_3))
 
             R_roll =  Matrix([[ 1,         0,          0, 0],
                               [ 0, cos(roll), -sin(roll), 0],
@@ -198,12 +195,14 @@ def handle_calculate_IK(req):
                               [ sin(yaw),  cos(yaw),    0, 0],
                               [        0,         0,    1, 0],
                               [        0,         0,    0, 1]])
-            Rrpy = R_roll * R_pitch * R_yaw * R_corr
-            print('Rrpy = ', simplify(Rrpy))
+
+            Rrpy = R_yaw * R_pitch * R_roll * R_corr
+            #print'Rrpy = ', simplify(Rrpy)
 	    #print("\n")
            
             R3_6 = R0_3.inv() * Rrpy  #R0_3**-1
-            print('R3_6 = ', simplify(R3_6))
+            #print'R3_6 = ', simplify(R3_6)
+	    #print("\n")
 
             r11 = R3_6[0,0]
             r12 = R3_6[0,1]
@@ -214,22 +213,21 @@ def handle_calculate_IK(req):
             r31 = R3_6[2,0]
             r32 = R3_6[2,1]
             r33 = R3_6[2,2]
-            print(r21, r11, r31)
 
-            theta4 = atan2(-r33, r13) #r21, r11  r33, -r13
-            th4deg = theta4*rtd
-	    print('theta4 = ', simplify(th4deg))
+            theta4 = atan2(r33, -r13) 
+            #th4deg = theta4*rtd
+	    #print'theta4 = ', simplify(th4deg)
 
-            theta5 = atan2(-r31, sqrt(r11**2 + r21**2)) #r23, sqrt(r21**2 + r22**2) -r31, sqrt(r11**2 + r21**2) -r13, sqrt(1+r13**2)
-            th5deg = theta5*rtd
-	    print('theta5 = ', simplify(th5deg))
+            theta5 = atan2(sqrt(r13**2 + r33**2),r23) 
+            #th5deg = theta5*rtd
+	    #print'theta5 = ', simplify(th5deg)
 
-            theta6 = atan2(r22, -r21) #r32, r33  -r22, r21
-            th6deg = theta6*rtd
-	    print('theta6 = ', simplify(th6deg))
+            theta6 = atan2(-r22, r21) 
+            #th6deg = theta6*rtd
+	    #print'theta6 = ', simplify(th6deg)
 
-            #print "FW Kinematics Result:"
-            #print(T_total.evalf(subs={q1:theta1, q2:theta2, q3:theta3, q4:theta4, q5:theta5, q6:theta6}))
+            print "FW Kinematics Result:"
+            print(T_total.evalf(subs={q1:theta1, q2:theta2, q3:theta3, q4:theta4, q5:theta5, q6:theta6}))
 
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
